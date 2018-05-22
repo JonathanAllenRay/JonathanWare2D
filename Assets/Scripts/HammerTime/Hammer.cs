e9887118fd008add88a861958c546f3b6b3ba307
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-22 14:29:59 -0500

    Added Hammer Time, almost done. Changed earf asset to a much compressed version

---
+++ Assets/Scripts/HammerTime/Hammer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    private bool moving = false;
+    public BoxCollider2D bc;
+
+    private float forceBonus = 3000.0f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb.bodyType != RigidbodyType2D.Dynamic && Input.GetMouseButtonDown(0))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+        if (SwipeManager.Instance.GetDir() == SwipeDirection.Left)
+        {
+            rb.AddForce(-transform.right * forceBonus);
+        }
+        else if (SwipeManager.Instance.GetDir() == SwipeDirection.Right)
+        {
+            rb.AddForce(transform.right * forceBonus);
+        }
+        SetMoving();
+    }
+
+    private void SetMoving()
+    {
+
+        if (Mathf.Abs(rb.angularVelocity) > 300.0f)
+        {
+            moving = true;
+            bc.isTrigger = true;
+        }
+        else
+        {
+            moving = false;
+            bc.isTrigger = false;
+        }
+    }
+}
---
+++ Assets/Scripts/HammerTime/HammerAlien.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerAlien : MonoBehaviour {
+
+    public float speed;
+    public bool movingRight;
+
+    public GameObject splatter;
+    public GameObject bang;
+
+
+    // Update is called once per frame
+    void Update() {
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.gameObject.tag == "Hammer")
+        {
+            Instantiate(splatter, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instantiate(bang, collision.transform.gameObject.transform.position, collision.transform.gameObject.transform.rotation);
+            Destroy(collision.transform.gameObject);
+        }
+    }
+}
