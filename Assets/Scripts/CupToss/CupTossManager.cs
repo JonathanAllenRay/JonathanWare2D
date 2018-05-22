f8d1149dc7182d106966c8784772d825611adcc7
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-22 12:01:34 -0500

    Added cup toss game

---
+++ Assets/Scripts/CupToss/CupTossManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupTossManager : MonoBehaviour {
+
+    private bool success = false;
+
+    public GameObject can;
+
+    public float time;
+    private bool ended = false;
+    public GameObject textTimer;
+    public float timeScaleMod;
+    // Use this for initialization
+    void Start () {
+        Time.timeScale += timeScaleMod;
+        float xTemp = can.transform.position.x;
+        can.transform.position = new Vector2(Random.Range(xTemp - 1.0f, xTemp + 1.0f), can.transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            textTimer.GetComponent<UnityEngine.UI.Text>().text = "Outta Time";
+            if (!ended)
+            {
+                Debug.Log(success);
+                Result();
+            }
+            ended = true;
+        }
+        else
+        {
+            int roundedSeconds = (int)time;
+            textTimer.GetComponent<UnityEngine.UI.Text>().text = "Time Left: " + roundedSeconds;
+        }
+    }
+
+    private void Result()
+    {
+
+    }
+
+    public void InTheCan()
+    {
+        success = true;
+        Debug.Log("In the can");
+    }
+
+
+}
---
+++ Assets/Scripts/CupToss/TossCup.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TossCup : MonoBehaviour {
+
+    public Rigidbody2D cupRb;
+    public SpriteRenderer cupSr;
+
+    public BoxCollider2D bcBottom;
+    public BoxCollider2D canSuccess;
+
+    public BoxCollider2D side1;
+    public BoxCollider2D side2;
+
+    public CupTossManager ctm;
+
+
+
+    private bool thrown = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!thrown)
+        {
+            if (SwipeManager.Instance.GetDir() != SwipeDirection.None)
+            {
+                thrown = true;
+                cupRb.gravityScale = 0.8f;
+                cupRb.AddForce(Vector3.Normalize(SwipeManager.Instance.GetSwipeVector())*1400.0f);
+                cupRb.AddTorque(Random.Range(-30.0f, 30.0f));
+            }
+        }
+        if (thrown)
+        {
+            if (transform.localScale.x >= .9f)
+            {
+                transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.gameObject.tag == "FloorTrigger")
+        {
+            bcBottom.enabled = true;
+        }
+        else if (collision.transform.gameObject.tag == "SuccessBox")
+        {
+            ctm.InTheCan();
+        }
+        else
+        {
+            cupSr.sortingOrder = 2;
+            side1.enabled = true;
+            side2.enabled = true;
+            canSuccess.enabled = true;
+        }
+    }
+}
