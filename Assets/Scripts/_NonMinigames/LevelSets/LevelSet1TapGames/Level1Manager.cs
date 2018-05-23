4ca6418e4c08b1c91ad3a700a8429dfa56ff6bb6
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-23 15:57:59 -0500

    Added setup for level 1 demo

---
+++ Assets/Scripts/_NonMinigames/LevelSets/LevelSet1TapGames/Level1Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Manager : MonoBehaviour {
+
+    private int lives = 3;
+    private int currentGameIndex;
+    private List<int> usedGameIndices;
+    private int gamesPlayed;
+    private int gamesToPlay = 10;
+
+    private bool endless = false;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/_NonMinigames/UIMenu/UIButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartLevel1()
+    {
+        SceneManager.LoadScene("Scenes/LevelSets/Level1TapGames");
+    }
+}
