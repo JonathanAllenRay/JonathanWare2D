cbfc0d6fc4b25975106299895ad8d84d01bd89ff
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-23 12:00:46 -0500

    Added Box Escape game under Accel/Gyro games

---
+++ Assets/Scripts/BoxEscape/BoxEscapeManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxEscapeManager : MonoBehaviour {
+
+    private GameObject[] exitBarriers;
+
+    public GameObject ballGameObject;
+    private EscapeBall ball;
+
+    public float time;
+    private bool ended = false;
+    public GameObject textTimer;
+    public float timeScaleMod;
+
+    private bool success = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        ball = ballGameObject.GetComponent<EscapeBall>();
+        ballGameObject.transform.position = new Vector2(ballGameObject.transform.position.x + Random.Range(-3.0f, 3.0f), 
+            ballGameObject.transform.position.y);
+        Time.timeScale += timeScaleMod;
+        exitBarriers = GameObject.FindGameObjectsWithTag("Barrier");
+        exitBarriers[Random.Range(0, exitBarriers.Length)].SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Physics2D.gravity = 30.0f * Input.acceleration.normalized;
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            textTimer.GetComponent<UnityEngine.UI.Text>().text = "Outta Time";
+            if (ball.HasEscaped())
+            {
+                success = true;
+            }
+            if (!ended)
+            {
+                Debug.Log(success);
+                Result();
+            }
+            ended = true;
+        }
+        else
+        {
+            int roundedSeconds = (int)time;
+            textTimer.GetComponent<UnityEngine.UI.Text>().text = "Time Left: " + roundedSeconds;
+        }
+    }
+
+    private void Result()
+    {
+
+    }
+}
---
+++ Assets/Scripts/BoxEscape/EscapeBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeBall : MonoBehaviour {
+
+
+    private bool hasEscaped = false;
+
+    public bool HasEscaped()
+    {
+
+        return hasEscaped;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        hasEscaped = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        hasEscaped = true;
+    }
+}
