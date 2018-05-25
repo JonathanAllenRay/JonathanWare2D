8e47ed97c242b9cd411e39f8ff7de051bda05096
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-25 14:12:25 -0500

    Added Boss Fight for Swipe, incomplete

---
+++ Assets/Scripts/BossFightSwipe/BossMonsterSwipe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMonsterSwipe : MonoBehaviour {
+
+    public float speed;
+    public GameObject flash;
+    private int health = 3;
+
+    public GameObject deathEffect;
+    //public SwipeBossFightManager sbfm;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        health--;
+        flash.SetActive(true);
+        Invoke("UndoFlash", .1f);
+        if (health <= 0)
+        {
+
+        }
+    }
+
+    void UndoFlash()
+    {
+        flash.SetActive(false);
+    }
+}
