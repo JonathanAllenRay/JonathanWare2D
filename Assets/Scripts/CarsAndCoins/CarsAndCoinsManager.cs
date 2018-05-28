9b3304fc80da9c83fb109c3b7ba25beebc751940
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-28 17:55:30 -0500

    Added Cars and Coins

---
+++ Assets/Scripts/CarsAndCoins/CarsAndCoinsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarsAndCoinsManager : MinigameManager {
+
+    private bool success = false;
+    public int coins = 3;
+    public GameObject coin;
+
+    private void Start()
+    {
+        for (int i = 0; i < coins; i++)
+        {
+            Instantiate(coin, new Vector2(Random.Range(-7f, 7f), Random.Range(0f, 3f)), transform.rotation);
+        }        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        GameTimeUpdate(success);
+	}
+
+    public void GetCoin()
+    {
+        coins--;
+        if (coins <= 0)
+        {
+            success = true;
+        }
+    }
+}
---
+++ Assets/Scripts/CarsAndCoins/CoinsCar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsCar : MonoBehaviour {
+
+    public float speed;
+    public CarsAndCoinsManager cacm;
+
+	// Update is called once per frame
+	void Update () {
+        float turnSpeed = Input.acceleration.x;
+        if (turnSpeed < -.3f)
+        {
+            turnSpeed = -.3f;
+        }
+        else if (turnSpeed > .3f)
+        {
+            turnSpeed = .3f;
+        }
+        transform.Rotate(-Vector3.forward * turnSpeed * Time.deltaTime * 800f);
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        cacm.GetCoin();
+        Destroy(collision.transform.gameObject);
+    }
+}
