ea02697f286bd6a23756b153101ebe0031e4879e
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-29 19:55:16 -0500

    Added Stop and Go under Touch and Hold category

---
+++ Assets/Scripts/StopAndGo/StopAndGoManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LightColor
+{
+    Red, Yellow, Green, None
+}
+
+public class StopAndGoManager : MinigameManager {
+
+    public StopAndGoRunner runner;
+
+    public GameObject green;
+    public GameObject yellow;
+    public GameObject red;
+
+    private LightColor color = LightColor.Green;
+
+    private bool aboutToStop = true;
+
+    private bool success = false;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("NextLight", Random.Range(1.0f, 1.2f), Random.Range(.5f, .8f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (color == LightColor.Red)
+        {
+            red.SetActive(true);
+            green.SetActive(false);
+            yellow.SetActive(false);
+        }
+        else if (color == LightColor.Yellow)
+        {
+            red.SetActive(false);
+            green.SetActive(false);
+            yellow.SetActive(true);
+        }
+        else if (color == LightColor.Green)
+        {
+            red.SetActive(false);
+            green.SetActive(true);
+            yellow.SetActive(false);
+        }
+        GameTimeUpdate(success);
+
+	}
+
+    void NextLight()
+    {
+        if (aboutToStop)
+        {
+            if (color == LightColor.Green)
+            {
+                color = LightColor.Yellow;
+            }
+            else if (color == LightColor.Yellow)
+            {
+                color = LightColor.Red;
+                aboutToStop = false;
+            }
+        }
+        else
+        {
+            if (color == LightColor.Red)
+            {
+                color = LightColor.Green;
+                aboutToStop = true;
+            }
+        }
+    }
+
+    public LightColor GetColor()
+    {
+        return color;
+    }
+
+    public void MadeIt()
+    {
+        success = true;
+    }
+
+    public float GetTime()
+    {
+        return time;
+    }
+}
---
+++ Assets/Scripts/StopAndGo/StopAndGoRunner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAndGoRunner : MonoBehaviour {
+
+    public Animator ani;
+
+    public float speed = 3.0f;
+
+    public GameObject explosion;
+
+    public StopAndGoManager sagm;
+
+    private bool localSuccess = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (!Input.GetMouseButton(0))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            ani.speed = 1f;
+            if (sagm.GetColor() == LightColor.Red && localSuccess == false)
+            {
+                Detonate();
+            }
+        }
+        else
+        {
+            ani.speed = 0f;
+        }
+        if (!localSuccess && sagm.GetTime() < 0f)
+        {
+            Detonate();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        sagm.MadeIt();
+        localSuccess = true;
+    }
+
+    public void Detonate()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
