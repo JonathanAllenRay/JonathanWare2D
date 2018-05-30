90df93656ab00a8add17cf683af09e50cfe534f3
JonathanAllenRay <jonathanallenray1@gmail.com>
2018-05-29 23:27:43 -0500

    Added MazeDrag game

---
+++ Assets/Scripts/MazeDrag/MazeDragManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeDragManager : MinigameManager {
+
+    public GameObject[] obstacles;
+
+    private bool success = false;
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale += timeScaleMod;
+        obstacles = GameObject.FindGameObjectsWithTag("Barrier");
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            obstacles[i].transform.Translate(Vector2.up * Random.Range(-2f, 2f));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameTimeUpdate(success);
+	}
+
+    public void MadeIt()
+    {
+        success = true;
+    }
+
+    public void Dead()
+    {
+        success = false;
+    }
+}
---
+++ Assets/Scripts/MazeDrag/MazeSnowman.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeSnowman : MonoBehaviour {
+
+    public MazeDragManager mdm;
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDrag()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.gameObject.tag == "SuccessBox")
+        {
+            mdm.MadeIt();
+        }
+        else
+        {
+            Destroy(gameObject);
+            Instantiate(explosion, transform.position, transform.rotation);
+            mdm.Dead();
+        }
+    }
+}
